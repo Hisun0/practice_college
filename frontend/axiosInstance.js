6203c07c1f4be92b34f6e26651dbbf076a3dc23b
Hisun0 <tridvad1@gmail.com>
2024-06-20 21:38:09 +0300

    little fix

--- frontend/axiosInstance.js
+++ frontend/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'http://158.160.89.12:3000/',
+  baseURL: process.env.NODE_ENV === 'production' ? 'http://158.160.89.12:3000/' : 'http://localhost:3000/',
   headers: {'Content-Type': 'application/json'},
   withCredentials: false
 });
