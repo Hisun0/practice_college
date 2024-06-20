ce52f6d750b362cd8ebd9e59c075ed0c611e7b37
Hisun0 <tridvad1@gmail.com>
2024-06-20 17:54:42 +0000

    prod working

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser('secret'));
   app.enableCors({
-    origin: 'https://champion-ladybird-formally.ngrok-free.app',
+    origin: 'http://158.160.89.12:3001',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
