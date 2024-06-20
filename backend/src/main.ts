c24c4e2a4cfa3baa39f4ee9a9f500729868acc58
Hisun0 <tridvad1@gmail.com>
2024-06-20 16:28:38 +0300

    add https for backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 import * as fs from 'node:fs';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import { cwd } from 'node:process';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(`${cwd()}/etc/nginx/certificate/key.pem`),
+    cert: fs.readFileSync(`${cwd()}/etc/nginx/certificate/cert.pem`),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.use(cookieParser('secret'));
   app.enableCors({
     origin: 'https://mentally-prime-possum.ngrok-free.app',
